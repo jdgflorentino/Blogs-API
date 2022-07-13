375fc82f96da1f65ef19de0fcbb00febb3b59fb6
Jéssica Florentino <jdgflorentino@gmail.com>
2022-07-13 17:53:07 -0300

    feat: Create migration PostCategories

---
+++ src/database/migrations/20220713204839-PostCategories.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+      },
+      categoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+      },
+      }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
