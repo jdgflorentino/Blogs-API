6467fa3166720ecf3e5fc69582072cbbafe8809d
Jéssica Florentino <jdgflorentino@gmail.com>
2022-07-13 17:48:05 -0300

    feat: Create migration BlogPosts

---
+++ src/database/migrations/20220713202625-BlogPost.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      userId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field:'published',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field:'updated',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+
+  }
+};
