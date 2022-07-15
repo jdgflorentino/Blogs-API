f9e38b9e685a395d1ad4fc2bba57a9af3543449d
Jéssica Florentino <jdgflorentino@gmail.com>
2022-07-15 16:28:09 -0300

    fix(user): Add timestamps
    Adicionai na linha 22 timestamps: false, pois estava dando erro no retorno do db

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,36 +1,33 @@
-const User = (sequelize, DataTypes) => { 
-  const Users = sequelize.define('User', {
+module.exports = (sequelize, DataTypes) => { 
+  const User = sequelize.define('User', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true
     },
     displayName: {
       type: DataTypes.STRING,
-      allowNull: false
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
     },
     image: {
       type: DataTypes.STRING,
     },
+
+  }
+    , {
+      timestamps: false,
   });
 
   User.associate = (models) => {
-    User.hasMany(models.BlogPosts, {
+    User.hasMany(models.BlogPost, {
       foreignKey: 'userId',
       as: 'posts'
     });
   }
 
-  return Users;
+  return User;
 };
-
-module.exports = User;
