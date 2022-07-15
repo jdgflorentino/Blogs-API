2ecd99eec8ea741db54c5b404be6781145f98023
Jéssica Florentino <jdgflorentino@gmail.com>
2022-07-15 16:59:46 -0300

    feat: Rotas no api

--- src/api.js
+++ src/api.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const userRoutes = require('./routes/user.routes');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use(userRoutes);
 
 // ...
 
