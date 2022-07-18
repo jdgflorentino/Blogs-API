e1653c82d0e24826697800d819fd377f3c1a5124
Jéssica Florentino <jdgflorentino@gmail.com>
2022-07-18 16:47:03 -0300

    feat(user): Route POST /user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const controller = require('../controllers/login.controller');
+const controller = require('../controllers/index');
 
 const userRouter = Router();
 
 userRouter.post('/login', controller.login);
+userRouter.post('/user', controller.register);
 
 module.exports = userRouter;
