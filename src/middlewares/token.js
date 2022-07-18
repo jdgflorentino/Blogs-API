f40f5e994abac85373edab9c36b2100bf2168489
Jéssica Florentino <jdgflorentino@gmail.com>
2022-07-18 17:22:45 -0300

    feat(user): Add verifyToken
    Criei função para validar o Token

--- src/middlewares/token.js
+++ src/middlewares/token.js
@@ -1,9 +1,23 @@
 const jwt = require('jsonwebtoken');
 
+const verifyToken = async (req, res, next) => { 
+  const { authorization } = req.headers;
+  if (!authorization) { 
+    return res.status(401).json({ message: 'Token not found' });
+  }
+  try { 
+    const payload = await jwt.verify(authorization, process.env.JWT_SECRET);
+    req.user = payload;
+    next();
+  } catch (error) { 
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+}; 
+
 const createToken = (email) => { 
   const token = jwt.sign({ data: email }, process.env.JWT_SECRET,
     { expiresIn: '1h', algorithm: 'HS256' });
   return token;
 };
 
-module.exports = { createToken };
+module.exports = { createToken, verifyToken };
