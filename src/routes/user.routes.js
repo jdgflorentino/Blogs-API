b205c5d5cb1517ea411532b165bf5cb4115fbf27
Jéssica Florentino <jdgflorentino@gmail.com>
2022-07-19 14:52:09 -0300

    feat(user): Create Route GET /user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const controller = require('../controllers/index');
+const { verifyToken } = require('../middlewares/token');
 
 const userRouter = Router();
 
 userRouter.post('/login', controller.login);
 userRouter.post('/user', controller.register);
+userRouter.get('/user', verifyToken, controller.getAllUser);
 
 module.exports = userRouter;
